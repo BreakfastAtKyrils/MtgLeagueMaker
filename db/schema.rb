3b9b266930413984d28b31b961c15f2d09c6e220
BreakfastAtKyrils <kyrildr@gmail.com>
2023-02-05 11:24:38 -0500

    create schema file for players, decks, games, game_records

---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+ActiveRecord::Schema[7.0].define do
+  create_table "players", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer :player_id, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.datetime "played_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "game_records", force: :cascade do |t|
+    t.integer :player_id, null: false
+    t.integer :game_id, null: false
+    t.string "record_type", null: false
+    t.integer :game_id, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+end
