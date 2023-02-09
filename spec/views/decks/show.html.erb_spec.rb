4cb1a868e519f9a60dcf4218477bde45f2c6e2b8
BreakfastAtKyrils <kyrildr@gmail.com>
2023-02-09 15:06:31 -0500

    delete unnecessary files + lint fix

--- spec/views/decks/show.html.erb_spec.rb
+++ spec/views/decks/show.html.erb_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe "decks/show.html.erb", type: :view do
+RSpec.describe 'decks/show.html.erb' do
   pending "add some examples to (or delete) #{__FILE__}"
 end
