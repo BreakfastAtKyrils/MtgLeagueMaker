b16cf77a02130d68ea5d0a6cd10963e0b5ee0692
BreakfastAtKyrils <kyrildr@gmail.com>
2023-02-11 19:40:50 -0500

    linter

--- spec/requests/get_games_spec.rb
+++ spec/requests/get_games_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'GET /games' do
   context 'when adding a game to the database' do
     it 'returns a non-empty array' do
       # I know the following variable is not used, but I want to make sure the db has 1 game record for this test
-      game = create(:game, played_at: DateTime.now.strftime('%B %d, %Y'))
+      Game.create(played_at: DateTime.now.strftime('%B %d, %Y'))
 
       get '/games.json'
       body = JSON.parse(response.body, symbolize_names: true)
