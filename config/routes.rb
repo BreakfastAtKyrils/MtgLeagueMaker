3f3739abacde03a5791c05f756e1dac10d279843
BreakfastAtKyrils <kyrildr@gmail.com>
2023-02-20 15:01:18 -0500

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'players#index'
-  delete 'players/:id', to: 'players#destroy'
   resources :players, only: %i[create index new show edit update destroy]
   resources :decks, only: %i[index show]
   resources :games, only: %i[index]
