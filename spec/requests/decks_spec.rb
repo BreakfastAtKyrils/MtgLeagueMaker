2347ae92641ef21a16b2e2c4210123e8870b7264
BreakfastAtKyrils <kyrildr@gmail.com>
2023-02-22 18:34:49 -0500

    refactoring

--- spec/requests/decks_spec.rb
+++ spec/requests/decks_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'decks' do
   it_behaves_like 'a get request' do
-    let(:player) { Player.create(name: 'Jules') }
-    let(:record) { Deck.create(name: 'Nissa', player: player) }
+    let(:record) { create(:deck) }
     let(:path) { "/decks/#{record.id}.json" }
     let(:expected_json_attributes) do
-      { deck: hash_including(name: 'Nissa') }
+      { deck: hash_including(name: 'Isamaru') }
     end
   end
 end
